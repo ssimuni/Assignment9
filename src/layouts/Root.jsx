040c9ecc976bf46d0a97817d41982a5720c71928
unknown <ssimuni>
2024-04-14 23:12:23 +0600

    setup done

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 
 const Root = () => {
@@ -8,6 +9,7 @@ const Root = () => {
     <div>
      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
